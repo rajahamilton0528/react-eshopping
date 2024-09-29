01844cf3cb37d848c0ef1d071ac802be25054736
rajahamilton0528 <rajahamilton0528@gmail.com>
2024-09-28 21:34:02 -0400

    Make sure brand header name with logo is clickable

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -300,12 +300,13 @@ function ProductList() {
             <div className="navbar" style={styleObj}>
                 <div className="tag">
                     <div className="luxury">
-                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                        
                         <a href="/" style={{ textDecoration: 'none' }}>
-                            <div>
+                            <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                            <span className="luxury-name">
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
-                            </div>
+                            </span>
                         </a>
                     </div>
                 </div>
